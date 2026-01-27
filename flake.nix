951f84c87c41bda66a0b0da6f499749da1a51c34
Ali Foroughi <foroughi.ali@gmail.com>
2026-01-28 00:00:36 +0100

    added flake project

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "lazy git repo";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+
+          buildInputs = with pkgs; [
+              pkgs.go           
+          ];
+
+          shellHook = ''
+          '';
+
+          NIX_NAME = "Lazy Git";
+        };
+      });
+
+}
